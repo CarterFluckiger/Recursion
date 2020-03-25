eccc216f59cf8763994b5302b549a21a66544b1a
Carter Fluckiger <fluckiger2@tritions.iowacentral.edu>
2020-03-25 13:20:34 -0500

    Commented

--- Recursion/main.cpp
+++ Recursion/main.cpp
@@ -12,6 +12,11 @@ void recursion(Stack Program , Stack New_Program);
 
 int main()
 {
+    //
+    // Just some code to test the code
+    // The code is arbitrary and can be changed
+    //
+    
     Stack Program;
     Stack New_Program;
     
@@ -26,20 +31,27 @@ int main()
     Program.Push(4);
     Program.Push(34);
     Program.Push(99);
+    Program.Pop();
+    Program.Push(98);
 
     recursion(Program, New_Program);
-    
+
     return 0;
 }
 
 void recursion(Stack Program,Stack New_Program)
 {
+    //Checks to see if it is empty so it can skip it if it is
      if(!Program.IsEmpty())
     {
+        //Puts top item of Program and pushes onto New Program
         New_Program.Push(Program.Top());
+        // Pops that number
         Program.Pop();
+        //Couts this number
         std::cout<<New_Program.Top()<<std::endl;
-        
+        //Checks to see if original program is empty
+        // Enters another recrusion if not empty
         if(!Program.IsEmpty())
         {
             recursion(Program,New_Program);
